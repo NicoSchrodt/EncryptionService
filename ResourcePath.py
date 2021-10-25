037107a537cd7a53da670b061a26b2fce535067c
NicoSchrodt <nico.schrodt@gmx.de>
2021-10-25 13:29:59 +0200

    Initital Commit

---
+++ ResourcePath.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+
+# takes a relative path and return an absolute path
+def resource_path(relative_path):
+    try:  # only work if python project was converted to exe file
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
