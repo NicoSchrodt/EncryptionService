45a247148b6ba4e589dd8fd68b42802063504981
NicoSchrodt <nico.schrodt@gmx.de>
2022-03-07 18:59:37 +0100

    Reformatted test/main-file

--- UnitTests/tests.py
+++ UnitTests/tests.py
@@ -8,35 +8,25 @@ from Service.Text.Vigeneretext import VigenereText
 
 
 class TestClass(unittest.TestCase):
-    '''
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-    '''
     def test_vigenere_encrypter(self):
+        # -------------------Mock
         mock = create_autospec(VigenereText)
         temp = string.ascii_uppercase
         character_list = []
         for i in range(len(temp)):
             character_list.append(temp[i])
         mock.get_eligible_characters.return_value = character_list
+        # -------------------Parameters
         mock.character_list = ['A', 'B', 'C', 'D']
         mock.cipher_character_list = []
         key = 'AAAA'
         expected_answer = ['A', 'B', 'C', 'D']
+        # -------------------Encrypt
         encrypter = VigenereEncrypter(mock)
         encrypter.encrypt(key)
         answer = mock.cipher_character_list
+
         self.assertEqual(expected_answer, answer)
 
 
--- main.py
+++ main.py
@@ -1,35 +1,9 @@
 from Service.Text.Vigeneretext import VigenereText
 from Service.Encryption.VigenereEncrypter import VigenereEncrypter
-from UnitTests.tests import TestClass
 
-
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    '''
-    text = VigenereText()
-    text.fill_character_list("Tes&t")
-    text.encrypt("EinSchluessel")
-    print(f'Cipher: {text.cipher_character_list}')
-    '''
-
     text2 = VigenereText()
     text2.fill_character_list("Tes&t")
     text2_enc = VigenereEncrypter(text2)
     text2_enc.encrypt("EinSchluessel")
     print(f'Cipher: {text2.cipher_character_list}')
-
-    test = TestClass()
-    test.test_vigenere_encrypter()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
