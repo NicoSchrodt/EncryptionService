361d3ce3e97b8ea4130704503a2255778419a964
Nico Schrodt <nico.schrodt@gmx.de>
2022-03-16 13:13:24 +0100

    Allows decrypt function to be used by MainWindow

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -6,6 +6,9 @@ from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtCore import Qt
 from PyQt6.uic import loadUi
 
+from Service.Encryption.VigenereEncrypter import VigenereEncrypter
+from Service.Text.Vigeneretext import VigenereText
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -18,7 +21,29 @@ class MainWindow(QMainWindow):
         exit_button = self.button_Exit
         exit_button.clicked.connect(self.close)
 
+        # Encrypt Button
+        encrypt_button = self.button_encrypt
+        encrypt_button.clicked.connect(self.encrypt)
+
+        # Decrypt Button
+        decrypt_button = self.button_decrypt
+        decrypt_button.clicked.connect(self.decrypt)
+
     def init_ui(self, ui_name):
         base_path = os.path.abspath(".")
         full_path = os.path.join(base_path, ui_name)
         loadUi(full_path, self)
+
+    def encrypt(self):
+        if self.comboBox_chiffre.currentText() == "Vigenère-Chiffre":
+            try:
+                text = VigenereText()
+                text.fill_character_list(self.clear_TE.toPlainText())
+                encrypter = VigenereEncrypter(text)
+                encrypter.encrypt(self.lineEdit_key.text())
+                self.cipher_TE.setPlainText(text.get_cipher_string())
+            except Exception as e:
+                print("Encryption Error: " + str(e))
+
+    def decrypt(self):
+        pass
