dc5666da1bb05f6d08d5479eace6d2770060250d
NicoSchrodt <nico.schrodt@gmx.de>
2022-03-16 16:49:18 +0100

    Deleted useless comments + imports + test-code

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import sys
-from UnitTests.tests import TestClass
+
 from PyQt6.QtWidgets import QApplication
 
-from Service.Text.Vigeneretext import VigenereText
-from Service.Encryption.VigenereEncrypter import VigenereEncrypter
 from GUI.MainWindow import MainWindow
 
-
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 if __name__ == '__main__':
-    text2 = VigenereText()
-    text2.fill_character_list("Tes&t")
-    text2_enc = VigenereEncrypter(text2)
-    text2_enc.encrypt("EinSchluessel")
-    print(f'Cipher: {text2.cipher_character_list}')
-
     app = QApplication(sys.argv)
     main_menu = MainWindow()
     main_menu.show()
     sys.exit(app.exec())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
