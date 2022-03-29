7a52dbcd402627e359ae89fa8c8b9a5c9f214dd6
NicoSchrodt <nico.schrodt@gmx.de>
2022-03-29 21:50:39 +0200

    Made use of Interface-Decorator, prevents use of subclasses that don't implement all methods.

--- Service/Encryption/EncrypterInterface.py
+++ Service/Encryption/EncrypterInterface.py
@@ -1,11 +1,16 @@
-class EncrypterInterface:
+import abc
+
+
+class EncrypterInterface(metaclass=abc.ABCMeta):
     def __init__(self, reference):
         self.text = reference
 
+    @abc.abstractmethod
     def encrypt(self, key):
         # Encrypt the character list in text-object
-        pass
+        raise NotImplementedError
 
+    @abc.abstractmethod
     def decrypt(self, key):
         # Decrypt the character list in text-object
-        pass
+        raise NotImplementedError
