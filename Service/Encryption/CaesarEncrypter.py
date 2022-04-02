7f172c18e8f0a70c8a11477fac25fce0d838ed86
NicoSchrodt <nico.schrodt@gmx.de>
2022-04-02 13:40:59 +0200

    Implemented CaesarEncrypter.

--- Service/Encryption/CaesarEncrypter.py
+++ Service/Encryption/CaesarEncrypter.py
@@ -6,9 +6,37 @@ class CaesarEncrypter(EncrypterInterface):
         super().__init__(reference=_reference)
 
     def encrypt(self, key):
-        # ToDo
-        pass
+        local_list = self.text.character_list
+        local_eligible_character_list = self.text.get_eligible_characters()
+        num_elg_chars = len(local_eligible_character_list)
+        for i in range(len(local_list)):
+            index_key = local_eligible_character_list.index(key)
+            try:
+                index_char = local_eligible_character_list.index(local_list[i])
+                index_new = index_char + index_key
+                while index_new >= num_elg_chars:
+                    index_new -= num_elg_chars
+                self.text.cipher_character_list.append(local_eligible_character_list[index_new])
+            except ValueError:
+                self.text.cipher_character_list.append(local_list[i])
+                if local_list[i] != " ":
+                    print("Invalid character '" + local_list[i] + "' found, skipped. "
+                                                                  "Please add a character-set which contains character.")
 
     def decrypt(self, key):
-        # ToDo
-        pass
+        local_list = self.text.cipher_character_list
+        local_eligible_character_list = self.text.get_eligible_characters()
+        num_elg_chars = len(local_eligible_character_list)
+        for i in range(len(local_list)):
+            index_key = local_eligible_character_list.index(key)
+            try:
+                index_char = local_eligible_character_list.index(local_list[i])
+                index_new = index_char - index_key
+                while index_new < 0:
+                    index_new += num_elg_chars
+                self.text.character_list.append(local_eligible_character_list[index_new])
+            except ValueError:
+                self.text.character_list.append(local_list[i])
+                if local_list[i] != " ":
+                    print("Invalid character '" + local_list[i] + "' found, skipped. "
+                                                                  "Please add a character-set which contains character.")
