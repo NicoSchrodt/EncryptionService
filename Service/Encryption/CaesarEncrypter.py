2c0018496534c388e7d1e29ce1b76ce7d8bd1a24
Nico Schrodt <nico.schrodt@gmx.de>
2022-04-06 08:44:54 +0200

    Check for key length in Caesar-Cipher + added character to special charset

--- Service/Encryption/CaesarEncrypter.py
+++ Service/Encryption/CaesarEncrypter.py
@@ -1,11 +1,23 @@
 from Service.Encryption.EncrypterInterface import EncrypterInterface
 
 
+def check_key_length(key, length):
+    if len(key) == length:
+        return True
+    else:
+        return False
+
+
 class CaesarEncrypter(EncrypterInterface):
     def __init__(self, _reference):
         super().__init__(reference=_reference)
+        self.key_length = 1
 
     def encrypt(self, key):
+        print(len(key))
+        if not check_key_length(key, self.key_length):
+            print("Invalid Key length!")
+            return
         key = key.upper()
         local_list = self.text.character_list
         local_eligible_character_list = self.text.get_eligible_characters()
@@ -25,6 +37,9 @@ class CaesarEncrypter(EncrypterInterface):
                                                                   "contains character.")
 
     def decrypt(self, key):
+        if not check_key_length(key, self.key_length):
+            print("Invalid Key length!")
+            return
         key = key.upper()
         local_list = self.text.cipher_character_list
         local_eligible_character_list = self.text.get_eligible_characters()
