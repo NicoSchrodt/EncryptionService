2e25a7f4ae461b0cfa3c65302a0eec4a8a00202a
NicoSchrodt <nico.schrodt@gmx.de>
2022-04-26 17:40:02 +0200

    Changes in Kasiski-Class Structure

--- Service/Analysis/Kasiski/GoBetween.py
+++ Service/Analysis/Kasiski/GoBetween.py
@@ -1,8 +1,9 @@
-from Service.Analysis.Kasiski.Kasiski import find_key_length
+from Service.Analysis.Kasiski.Kasiski import Kasiski
 from Service.Analysis.Helpers.const import (SEQ_LEN, MAX_KEY_LEN)
 
 
 def guessKey(ciphertext):
     key_len = 0
-    key_len = find_key_length(cyphertext=ciphertext, seq_len=SEQ_LEN, max_key_len=MAX_KEY_LEN)
+    KT = Kasiski()
+    key_len = KT.find_key_length(ciphertext=ciphertext, seq_len=SEQ_LEN, max_key_len=MAX_KEY_LEN)
     return key_len
